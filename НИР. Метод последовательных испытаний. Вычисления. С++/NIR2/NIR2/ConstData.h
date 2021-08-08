574426209d2b4b8837a26dfadd8eabb377d6e855
Alex <55331263+Guhabrus@users.noreply.github.com>
2021-08-08 15:04:35 +0300

    08.08.2021 15:04

---
+++ НИР. Метод последовательных испытаний. Вычисления. С++/NIR2/NIR2/ConstData.h
@@ -0,0 +1,17 @@
+#pragma once
+const int	 sT					= 100;
+const int    sX					= 40;
+const double HeatFluxRight		= 0.0;
+const int    X1					= 3;
+const double TempStart			= 200.0;
+const double Accuracy			= 1e-2;
+const double Emissivity			= 0.85;
+const double G					= 5.6703671313 / 100000000.0;
+const double AccuracuGradiend	= 0.000009;
+const double TimeCalc			= 300.0;
+
+const double del				= 0.0;
+const double M_PI				= 3.141592653589793;
+
+
+#define Min 0.00291884
